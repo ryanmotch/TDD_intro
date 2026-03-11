c39f14ca5a00a2be36cf6bb6052545e21b161c01
Ryan Motchkavitz <ryanmotch2@gmail.com>
2026-03-10 22:34:39 -0400

    js

--- index.js
+++ index.js
@@ -12,7 +12,10 @@
  * sumDouble(5, 5); // 20
  */
 export function sumDouble(num1, num2) {
-  /* TODO */
+  if (num1 === num2) {
+    return (num1 + num2) * 2;
+  }
+  return num1 + num2;
 }
 
 /**
@@ -28,8 +31,18 @@ export function sumDouble(num1, num2) {
  * makes10(4, 4); // false
  */
 export function makes10(num1, num2) {
-  /* TODO */
-}
+  if (num1 + num2 = 10){
+    return true
+  } else if (num1 = 10){
+    return true
+  } else if (num2 = 10){
+    return true
+  } else {
+    return false
+  }
+  }
+   
+
 
 /**
  * Hint: Math.abs(num) returns the absolute value of num.
@@ -46,7 +59,11 @@ export function makes10(num1, num2) {
  * near100(105, 10); // true
  */
 export function near100(n, distance) {
-  /* TODO */
+    // Calculate the absolute difference between n and 100
+  const difference = Math.abs(n - 100);
+
+  // Check if the difference is within the allowed distance
+  return difference <= distance;
 }
 
 /**
@@ -63,7 +80,11 @@ export function near100(n, distance) {
  * isMultiple35(7); // false
  */
 export function isMultiple35(n) {
-  /* TODO */
+  if (n % 3 === 0 || n % 5 === 0) {
+    return true
+  {else
+    return false
+  }
 }
 
 /**
@@ -81,7 +102,8 @@ export function isMultiple35(n) {
  * shareLastDigit(10, 21); // false
  */
 export function shareLastDigit(num1, num2) {
-  /* TODO */
+  return num1 % 10 === num2 % 10;
+   
 }
 
 /**
@@ -98,7 +120,12 @@ export function shareLastDigit(num1, num2) {
  * isColdAndHot(10, 50); // false
  */
 export function isColdAndHot(temp1, temp2) {
-  /* TODO */
+  if (temp1 < 0 && temp2 > 100)
+    return true
+  else if (temp2 < 0 && temp1 > 100)
+    return true
+  else
+    return false;
 }
 
 /**
@@ -114,7 +141,7 @@ export function isColdAndHot(temp1, temp2) {
  * makeABBA("a", "b"); // "abba"
  */
 export function makeABBA(A, B) {
-  /* TODO */
+  return A+B+B+A;
 }
 
 /**
@@ -135,7 +162,11 @@ export function makeABBA(A, B) {
  * makeSLS("a", "abc"); // "aabca"
  */
 export function makeSLS(str1, str2) {
-  /* TODO */
+  if (str1.length < str2.length) {
+    return str1 + str2 + str1;
+  } else {
+    return str2 + str1 + str2;
+}
 }
 
 /**
@@ -156,7 +187,12 @@ export function makeSLS(str1, str2) {
  * canEnterClub(5, 5); // 1
  */
 export function canEnterClub(you, date) {
-  /* TODO */
+  if (you || date >= 8)
+    return 2;
+  else if (you || date <= 2)
+    return 0;
+  else
+    return 1;
 }
 
 /**
@@ -177,5 +213,11 @@ export function canEnterClub(you, date) {
  * shouldAnswerPhone(true, true, false); // true
  */
 export function shouldAnswerPhone(isMorning, isBoss, isAsleep) {
-  /* TODO */
+  if (isAsleep) {
+    return false;
+  }else if (isMorning && !isBoss) {
+    return false;
+  }else{
+    return true;
+  }
 }
